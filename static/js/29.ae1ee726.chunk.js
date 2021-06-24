d41b6b23e84e53d5705ddb90e26a61b9ee200282
Enkh-Amar <enkhamar1220@gmail.com>
2021-06-24 10:03:57 +0800

    Updates

---
+++ static/js/29.ae1ee726.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[29],{647:function(e,s,n){"use strict";n.r(s);n(0);var t=n(1);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=29.ae1ee726.chunk.js.map
